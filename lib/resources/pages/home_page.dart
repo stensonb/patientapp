5ff3b226a2a503c41a05a0136128cf3a66e13a18
Anthony <ants52@aol.com>
2024-11-05 23:53:08 +0700

    v6.1.1

--- lib/resources/pages/home_page.dart
+++ lib/resources/pages/home_page.dart
@@ -19,12 +19,12 @@ class _HomePageState extends NyPage<HomePage> {
 
   @override
   get init => () async {
-        /// Uncomment the code below to fetch the number of stars for the Nylo repository
-        // Map<String, dynamic>? githubResponse = await api<ApiService>(
-        //         (request) => request.githubInfo(),
-        // );
-        // _stars = githubResponse?["stargazers_count"];
-      };
+    /// Uncomment the code below to fetch the number of stars for the Nylo repository
+    // Map<String, dynamic>? githubResponse = await api<ApiService>(
+    //         (request) => request.githubInfo(),
+    // );
+    // _stars = githubResponse?["stargazers_count"];
+  };
 
   /// Define the Loading style for the page.
   /// Options: LoadingStyle.normal(), LoadingStyle.skeletonizer(), LoadingStyle.none()
@@ -48,137 +48,125 @@ class _HomePageState extends NyPage<HomePage> {
       ),
       body: SafeAreaWidget(
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Logo(),
-          Text(
-            getEnv("APP_NAME"),
-          ).displayMedium(color: context.color.content),
-          const Text("Micro-framework for Flutter", textAlign: TextAlign.center)
-              .titleMedium(color: context.color.primaryAccent),
-          const Text("Build something amazing 💡", textAlign: TextAlign.center)
-              .bodyMedium()
-              .alignCenter(),
-          Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Divider(),
-              Container(
-                height: 250,
-                width: double.infinity,
-                margin:
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Logo(),
+              Text(
+                getEnv("APP_NAME"),
+              ).displayMedium(color: context.color.content),
+              const Text("Micro-framework for Flutter", textAlign: TextAlign.center)
+                  .titleMedium(color: context.color.primaryAccent),
+              const Text("Build something amazing 💡", textAlign: TextAlign.center)
+                  .bodyMedium()
+                  .alignCenter(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const Divider(),
+                  Container(
+                    height: 250,
+                    width: double.infinity,
+                    margin:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                padding:
+                    padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                decoration: BoxDecoration(
-                    color: context.color.surfaceBackground,
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 9,
-                        offset: const Offset(0, 3),
+                    decoration: BoxDecoration(
+                        color: context.color.surfaceBackground,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 1,
+                            blurRadius: 9,
+                            offset: const Offset(0, 3),
+                          ),
+                        ]),
+                    child: Center(
+                      child: ListView(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        children: ListTile.divideTiles(context: context, tiles: [
+                          if (Nylo.containsRoute("/landing"))
+                            ListTile(
+                              leading: FaIcon(FontAwesomeIcons.rocket),
+                              title: Text(
+                                "Start building",
+                              ).bodyLarge(color: context.color.surfaceContent),
+                              subtitle: Text(
+                                "Your project is ready",
+                              ).bodySmall(color: context.color.surfaceContent),
+                              trailing: const Icon(Icons.chevron_right),
+                              onTap: () => routeTo("/landing"),
+                            ),
+                          ListTile(
+                            leading: FaIcon(FontAwesomeIcons.readme),
+                            title: Text(
+                              "Documentation",
+                            ).bodyLarge(color: context.color.surfaceContent),
+                            subtitle: Text(
+                              "Master the framework",
+                            ).bodySmall(color: context.color.surfaceContent),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: widget.controller.onTapDocumentation,
+                          ),
+                          ListTile(
+                            leading: FaIcon(FontAwesomeIcons.github),
+                            title: Text(
+                              "Github",
+                            ).bodyLarge(color: context.color.surfaceContent),
+                            subtitle: Text(
+                              _stars == null ? "Source code" : "$_stars Stars",
+                            ).bodySmall(color: context.color.surfaceContent),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: widget.controller.onTapGithub,
+                          ),
+                          ListTile(
+                            leading: FaIcon(FontAwesomeIcons.newspaper),
+                            title: Text(
+                              "Updates",
+                            ).bodyLarge(color: context.color.surfaceContent),
+                            subtitle: Text(
+                              "View the changelog",
+                            ).bodySmall(color: context.color.surfaceContent),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: widget.controller.onTapChangeLog,
+                          ),
+                          ListTile(
+                            leading: FaIcon(FontAwesomeIcons.youtube),
+                            title: Text(
+                              "YouTube Channel",
+                            ).bodyLarge(color: context.color.surfaceContent),
+                            subtitle: Text(
+                              "Tutorial videos",
+                            ).bodySmall(color: context.color.surfaceContent),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: widget.controller.onTapYouTube,
+                          ),
+                          ListTile(
+                            leading: FaIcon(FontAwesomeIcons.xTwitter),
+                            title: Text(
+                              "Follow us on X",
+                            ).bodyLarge(color: context.color.surfaceContent),
+                            subtitle: Text(
+                              "Stay updated",
+                            ).bodySmall(color: context.color.surfaceContent),
+                            trailing: const Icon(Icons.chevron_right),
+                            onTap: widget.controller.onTapX,
+                          ),
+                        ]).toList(),
                       ),
-                    ]),
-                child: Center(
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    children: ListTile.divideTiles(context: context, tiles: [
-                      if (Nylo.containsRoute("/landing"))
-                        MaterialButton(
-                          onPressed: () => routeTo("/landing"),
-                          child: Text(
-                            "Landing".tr(),
-                          ).bodyLarge().setColor(
-                              context, (color) => color.surfaceContent),
-                        ),
-                      if (Nylo.containsRoutes(["/login", "/register"])) ...[
-                        MaterialButton(
-                          onPressed: () => routeTo("/login"),
-                          child: Text(
-                            "Login".tr(),
-                          ).bodyLarge().setColor(
-                              context, (color) => color.surfaceContent),
-                        ),
-                        MaterialButton(
-                          onPressed: () => routeTo("/register"),
-                          child: Text(
-                            "Register".tr(),
-                          ).bodyLarge().setColor(
-                              context, (color) => color.surfaceContent),
-                        ),
-                      ],
-                      ListTile(
-                        leading: FaIcon(FontAwesomeIcons.readme),
-                        title: Text(
-                          "Documentation",
-                        ).bodyLarge(color: context.color.surfaceContent),
-                        subtitle: Text(
-                          "Master the framework",
-                        ).bodySmall(color: context.color.surfaceContent),
-                        trailing: const Icon(Icons.chevron_right),
-                        onTap: widget.controller.onTapDocumentation,
-                      ),
-                      ListTile(
-                        leading: FaIcon(FontAwesomeIcons.github),
-                        title: Text(
-                          "Github",
-                        ).bodyLarge(color: context.color.surfaceContent),
-                        subtitle: Text(
-                          _stars == null ? "Source code" : "$_stars Stars",
-                        ).bodySmall(color: context.color.surfaceContent),
-                        trailing: const Icon(Icons.chevron_right),
-                        onTap: widget.controller.onTapGithub,
-                      ),
-                      ListTile(
-                        leading: FaIcon(FontAwesomeIcons.newspaper),
-                        title: Text(
-                          "Updates",
-                        ).bodyLarge(color: context.color.surfaceContent),
-                        subtitle: Text(
-                          "View the changelog",
-                        ).bodySmall(color: context.color.surfaceContent),
-                        trailing: const Icon(Icons.chevron_right),
-                        onTap: widget.controller.onTapChangeLog,
-                      ),
-                      ListTile(
-                        leading: FaIcon(FontAwesomeIcons.youtube),
-                        title: Text(
-                          "YouTube Channel",
-                        ).bodyLarge(color: context.color.surfaceContent),
-                        subtitle: Text(
-                          "Tutorial videos",
-                        ).bodySmall(color: context.color.surfaceContent),
-                        trailing: const Icon(Icons.chevron_right),
-                        onTap: widget.controller.onTapYouTube,
-                      ),
-                      ListTile(
-                        leading: FaIcon(FontAwesomeIcons.xTwitter),
-                        title: Text(
-                          "Follow us on X",
-                        ).bodyLarge(color: context.color.surfaceContent),
-                        subtitle: Text(
-                          "Stay updated",
-                        ).bodySmall(color: context.color.surfaceContent),
-                        trailing: const Icon(Icons.chevron_right),
-                        onTap: widget.controller.onTapX,
-                      ),
-                    ]).toList(),
+                    ),
                   ),
-                ),
+                  const Text(
+                    "Framework Version: $nyloVersion",
+                  ).bodyMedium().setColor(context, (color) => Colors.grey),
+                  ThemeToggle(),
+                ],
               ),
-              const Text(
-                "Framework Version: $nyloVersion",
-              ).bodyMedium().setColor(context, (color) => Colors.grey),
-              ThemeToggle(),
             ],
-          ),
-        ],
-      )),
+          )),
     );
   }
 }
